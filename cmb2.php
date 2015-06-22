b07174de4680cabbd19c5db210630f82324a5a48
Matt Gibbs <mgibbs189@users.noreply.github.com>
2015-06-22 17:17:45 -0400

    Update cmb2.php

--- cmb2.php
+++ cmb2.php
@@ -25,11 +25,12 @@ class FacetWP_Integration_CMB2
             'choices' => array(),
         );
 
+        // Get every CMB2 registered field as an array
         $fields = $this->get_fields();
 
         foreach ( $fields as $field ) {
-            $field_id = 'unique field id';
-            $field_label = 'the field label';
+            $field_id = $field['id'];
+            $field_label = $field['label'];
             $sources['cmb2']['choices'][ "cmb2/$field_id" ] = $field_label;
         }
 
@@ -45,101 +46,15 @@ class FacetWP_Integration_CMB2
         $facet = $params['facet'];
 
         if ( 'cmb2/' == substr( $facet['source'], 0, 4 ) ) {
-            // TODO index the 
+            // TODO index the value
+
+            // return TRUE to prevent the default indexer from running
             return true;
         }
 
         return $return;
     }
-
-
-    /**
-     * Handle advanced field types
-     */
-    function index_field_value( $value, $field, $params ) {
-
-        $value = maybe_unserialize( $value );
-
-        // checkboxes
-        if ( 'checkbox' == $field['type'] || 'select' == $field['type'] ) {
-            if ( false !== $value ) {
-                foreach ( (array) $value as $val ) {
-                    $display_value = isset( $field['choices'][ $val ] ) ?
-                        $field['choices'][ $val ] :
-                        $val;
-
-                    $params['facet_value'] = $val;
-                    $params['facet_display_value'] = $display_value;
-                    FWP()->indexer->index_row( $params );
-                }
-            }
-        }
-
-        // relationship
-        elseif ( 'relationship' == $field['type'] || 'post_object' == $field['type'] ) {
-            if ( false !== $value ) {
-                foreach ( (array) $value as $val ) {
-                    $params['facet_value'] = $val;
-                    $params['facet_display_value'] = get_the_title( $val );
-                    FWP()->indexer->index_row( $params );
-                }
-            }
-        }
-
-        // text
-        else {
-            $params['facet_value'] = $value;
-            $params['facet_display_value'] = $value;
-            FWP()->indexer->index_row( $params );
-        }
-    }
-
-
-    /**
-     * Get field settings
-     * @return array
-     */
-    function get_fields() {
-        $field_groups = acf_get_field_groups();
-        foreach ( $field_groups as $field_group ) {
-            $fields = acf_get_fields( $field_group );
-            $this->recursive_get_fields( $fields, $field_group, $hierarchy = '' );
-        }
-
-        return $this->fields;
-    }
-
-
-    /**
-     * Recursive handling for repeater fields
-     *
-     * We're storing a "hierarchy" string to figure out what
-     * values we need via get_field()
-     */
-    function recursive_get_fields( $fields, $field_group, $hierarchy ) {
-        foreach ( $fields as $field ) {
-
-            // append the hierarchy string
-            $new_hierarchy = $hierarchy . '/' . $field['key'];
-
-            // loop again for repeater fields
-            if ( 'repeater' == $field['type'] ) {
-                $this->recursive_get_fields( $field['sub_fields'], $field_group, $new_hierarchy );
-            }
-            else {
-                $this->fields[] = array(
-                    'key'           => $field['key'],
-                    'name'          => $field['name'],
-                    'label'         => $field['label'],
-                    'hierarchy'     => trim( $new_hierarchy, '/' ),
-                    'group_title'   => $field_group['title'],
-                );
-            }
-        }
-    }
 }
 
 
-if ( function_exists( 'acf' ) ) {
-    new FacetWP_Integration_CMB2();
-}
+new FacetWP_Integration_CMB2();
