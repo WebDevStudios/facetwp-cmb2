868d3e5842e5687096cd1d7d154e47c2e7115d00
Jeremy Pry <jeremy.pry@gmail.com>
2015-06-23 12:37:12 -0400

    Add plugin header

--- cmb2.php
+++ cmb2.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Plugin Name: CMB2 Integration for FacetWP
+ * Plugin URI:
+ * Description:
+ * Version: 1.0
+ * Author: Matt Gibbs, WebDevStudios
+ * License: GPL2
+ */
+
+// Prevent direct access to this file
+if ( ! defined( 'ABSPATH' ) ) {
+	die( "You can't do anything by accessing this file directly." );
+}
 
 class FacetWP_Integration_CMB2
 {
